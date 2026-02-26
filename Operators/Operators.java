fad6e9a69dad98647b9b85238d4941dbf4bca792
Ankur <tiwariankur2004@gmail.com>
2026-02-26 14:21:23 +0530

    Learning operators in Java

---
+++ Operators/Operators.java
@@ -0,0 +1,24 @@
+public class Operators{
+    public static void main(String[] args){
+        //Operators in Java
+        //Arithematic operators
+        int a=5;
+        int b=10;
+        int c=a+b; 
+        int d=a-b; //
+        int e=a*b;
+        int f=b/a;
+        int g=b%a;
+        System.out.println(c + " , " + d + " ," +e+ ", "+ f+" ,"+g);
+        int h=a+2;
+        h+=2;
+        System.out.println(h);
+        //pre /post increment and decrement 
+        int j=7;
+        j++;
+        ++j;
+        System.out.println(j);
+         
+
+    }
+}
